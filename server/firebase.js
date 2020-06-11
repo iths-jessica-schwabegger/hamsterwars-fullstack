c0f7b33e9b44475433d2945c179d2470054010b2
Jessica <jessica.schwabegger@hotmail.com>
2020-06-11 08:53:31 +0200

    env fixes

--- server/firebase.js
+++ server/firebase.js
@@ -3,8 +3,13 @@ var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccount.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://hamst3rwars.firebaseio.com"
+  credential: admin.credential.cert({
+    "project_id": "hamst3rwars",
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL
+  }),
+  databaseURL: process.env.DB_URL,
+  storageBucket: process.env.BUCKET
 });
 
 let db = admin.firestore();
