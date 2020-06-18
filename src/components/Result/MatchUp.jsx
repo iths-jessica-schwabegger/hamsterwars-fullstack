41ebe51abf93b272b1ce7aec5e81ef52d1997383
Jessica <jessica.schwabegger@hotmail.com>
2020-06-18 16:39:15 +0200

    fixed matchup

--- src/components/Result/MatchUp.jsx
+++ src/components/Result/MatchUp.jsx
@@ -9,9 +9,6 @@ const MatchUp = ({ match }) => {
     const [winnerId, setWinnerId] = useState("");
     const [hamsterOne, setHamsterOne] = useState("");
     const [hamsterTwo, setHamsterTwo] = useState("");
-    const [winningHamster, setWinningHamster] = useState("");
-    const [loosingHamster, setLoosingHamster] = useState("");
-
 
     const id1 = match.params.id1;
     const id2 = match.params.id2;
@@ -21,43 +18,33 @@ const MatchUp = ({ match }) => {
             async function getGames() {
                 let response = await fetch(`/games`);
                 const games = await response.json();
-                console.log(games);
+
                 const searchGame = games.games.filter(game => {
                     return game.contestants.id1 == id1 && game.contestants.id2 == id2 || game.contestants.id1 == id2 && game.contestants.id2 == id1;
                 })
 
                 setGame(searchGame);
+                setWinnerId(searchGame[0].winner.id);
             }
             getGames();
         
     }, []);
-
   
     useEffect(() => {
         if(game.length == 0) {
             console.log("That battle does not exist!")
         }else {
             console.log("Match found")
-            setWinnerId(game[0].winner.id);
 
             async function getContestants() {
 
                 const hamsterOne = await getHamsterById(match.params.id1);
                 const hamsterTwo = await getHamsterById(match.params.id2);
 
-                // setHamsterOne(hamsterOne.hamster);
-                // setHamsterTwo(hamsterTwo.hamster);
+                setHamsterOne(hamsterOne.hamster);
+                setHamsterTwo(hamsterTwo.hamster);
 
-            //Blir fel, hann inte fixa klart
-            if(hamsterOne.hamster.id === winnerId) {
-                setWinningHamster(hamsterOne.hamster);
-                setLoosingHamster(hamsterTwo.hamster);
-            }else{
-                setWinningHamster(hamsterTwo.hamster); 
-                setLoosingHamster(hamsterOne.hamster);
-            }
-
-            }
+         }
             getContestants();
         }
     }, [game]);
@@ -67,26 +54,26 @@ const MatchUp = ({ match }) => {
             <h1>Previous match results</h1>
             <section>
                 <article>
-                    <img src={"/images/" + winningHamster.imgName} alt="Cute hamster" />
-                    <p>{winningHamster.name}</p>
+                    <img src={"/images/" + (hamsterOne.id === winnerId ? hamsterOne.imgName : hamsterTwo.imgName)} alt="Cute hamster" />
+                    <p>{hamsterOne.id === winnerId ? hamsterOne.name : hamsterTwo.name}</p>
 
-                    <h5>About {winningHamster.name}</h5>
-                    <ul key={winningHamster.id}>
-                        <li>Age: {winningHamster.age}</li>
-                        <li>Loves to {winningHamster.loves}</li>
-                        <li>Favourite food: {winningHamster.favFood}</li>
+                    <h5>About {hamsterOne.id === winnerId ? hamsterOne.name : hamsterTwo.name}</h5>
+                    <ul key={hamsterOne.id}>
+                        <li>Age: {hamsterOne.id === winnerId ? hamsterOne.age : hamsterTwo.age}</li>
+                        <li>Loves to {hamsterOne.id === winnerId ? hamsterOne.loves : hamsterTwo.loves}</li>
+                        <li>Favourite food: {hamsterOne.id === winnerId ? hamsterOne.favFood : hamsterTwo.favFood}</li>
                     </ul>
                 </article>
                     <h3>DEFEATS</h3>
                 <article>
-                    <img src={"/images/" + loosingHamster.imgName} alt="Cute hamster" />
-                    <p>{loosingHamster.name}</p>
+                    <img src={"/images/" + (hamsterOne.id === winnerId ? hamsterTwo.imgName : hamsterOne.imgName)} alt="Cute hamster" />
+                    <p>{hamsterOne.id === winnerId ? hamsterTwo.name : hamsterOne.name}</p>
 
-                    <h5>About {loosingHamster.name}</h5>
-                    <ul key={loosingHamster.id}>
-                        <li>Age: {loosingHamster.age}</li>
-                        <li>Loves to {loosingHamster.loves}</li>
-                        <li>Favourite food: {loosingHamster.favFood}</li>
+                    <h5>About {hamsterOne.id === winnerId ? hamsterTwo.name : hamsterOne.name}</h5>
+                    <ul key={hamsterTwo.id}>
+                        <li>Age: {hamsterOne.id === winnerId ? hamsterTwo.age : hamsterTwo.One}</li>
+                        <li>Loves to {hamsterOne.id === winnerId ? hamsterTwo.loves : hamsterOne.loves}</li>
+                        <li>Favourite food: {hamsterOne.id === winnerId ? hamsterTwo.favFood : hamsterOne.favFood}</li>
                     </ul>
                 </article>
             </section>
